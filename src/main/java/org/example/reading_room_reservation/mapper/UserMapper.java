52f901b94d798fd11bc5b259a6b85f92f9f711e1
admin <k01088219732@gmail.com>
2024-10-16 16:37:57 +0900

    로그인 로직 수정

--- src/main/java/org/example/reading_room_reservation/mapper/UserMapper.java
+++ src/main/java/org/example/reading_room_reservation/mapper/UserMapper.java
@@ -1,15 +1,2 @@
-package org.example.reading_room_reservation.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.example.reading_room_reservation.entity.User;
-
-
-@Mapper
-public interface UserMapper {
-    User getUserById(int id);
-    User getUserByUsername(String username);
-    void insertUser(User user);
-    void updateUser(User user);
-    void deleteUser(int id);
+package org.example.reading_room_reservation.mapper;public class UserMapper {
 }
-
